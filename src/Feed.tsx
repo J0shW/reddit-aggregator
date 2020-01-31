99e16e2bc7f879ff68fbbfa083ef4c3c9a82bf81
Empiratus <joshwhitney47@gmail.com>
2020-01-30 22:22:17 -0700

    Added Semantic UI

--- src/Feed.tsx
+++ src/Feed.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronCircleUp, faChevronCircleDown } from '@fortawesome/free-solid-svg-icons';
 import Snoowrap from 'snoowrap';
+import { Item, Icon } from 'semantic-ui-react';
 
 import './Feed.scss';
 
@@ -12,27 +13,32 @@ interface FeedProps {
 const Feed: React.FC<FeedProps> = props => {
     const getThumbnail = (post: any) => {
         if (post.thumbnail && post.thumbnail.substring(0, 4) === 'http') {
-            return <img src={post.thumbnail} alt={post.title} />;
+            // return <img src={post.thumbnail} alt={post.title} />;
+            return <Item.Image size="small" src={post.thumbnail} />;
         }
     };
 
     const renderPosts = () => {
         return props.posts.map(post => {
             return (
-                <div key={post.id} className="feedPost">
+                <Item>
                     {getThumbnail(post)}
-                    <h3>{post.title}</h3>
-                    <h4>by: {post.author.name}</h4>
-                    <h5>Score: {post.score}</h5>
-                    <FontAwesomeIcon icon={faChevronCircleUp} style={{ fontSize: '25px', color: 'orange' }} />
-                    <FontAwesomeIcon icon={faChevronCircleDown} style={{ fontSize: '25px', color: 'mediumpurple' }} />
-                    <hr />
-                </div>
+                    <Item.Content>
+                        <Item.Header as="a">{post.title}</Item.Header>
+                        <Item.Description>by: {post.author.name}</Item.Description>
+                        <Item.Extra>
+                            <Icon color="orange" name="arrow up" />
+                            {post.ups}
+                            <Icon color="purple" name="arrow down" />
+                            {post.downs}
+                        </Item.Extra>
+                    </Item.Content>
+                </Item>
             );
         });
     };
 
-    return <main className="feed">{props.posts !== undefined ? renderPosts() : null}</main>;
+    return <Item.Group>{props.posts !== undefined ? renderPosts() : null}</Item.Group>;
 };
 
 export default Feed;
